dae6a098960e84ce75aac6b292295c4f6cef5d61
forxtoisate <hwoolford@gmail.com>
2024-04-09 17:54:41 -0500

    initial upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+// import { Outlet } from "react-router-dom";
+
+function App() {
+
+  return (
+    <div className='app-container'>
+        {/* NavBar */}
+        <div className='main-content'>
+        {/* Outlet */}
+        </div>
+        {/* Footer */}
+    </div>
+  )
+}
+
+export default App
